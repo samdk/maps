1382477456486752cbd2ca8e6c16caeaf2b6ab58
Sam DeFabbia-Kane <sam@defabbiakane.com>
2010-07-29 17:10:24 -0400

    Added some noise-removing/smoothing stuff.

--- index.js
+++ index.js
@@ -9,33 +9,23 @@ function drawMap() {
 		canvasData = ctx.createImageData(canvas.width, canvas.height),
 		MIN_SIZE = 1,
 		THRESHOLD = 0.40,
-		LAND_COLOR = [52,155,63],
+		LAND_COLOR = [[52,155,63],[151,155,52]],
 		BORDER_COLOR = [40,60,40],
 		WATER_COLOR = [60,88,231];
-
-	/* generates a random grid of 0s and 1s
-	 */
-	function randGrid(width,height) {
-		var arr = [];
-		for (var x = 0; x < width; x++) {
-			arr[x] = [];
-			for (var y = 0; y < height; y++) {
-				arr[x][y] = Math.random() < THRESHOLD ? 0 : 1;
-			}
-		}
-		return arr;
-	}
 
 	/* draws the map from a grid
 	 */
 	function drawGrid(grid) {
+		//grid = cleanBorders(borderize(grid));
+		grid = cleanBorders(borderize(smooth(grid,1)));
 		for (var x = 0; x < grid.length; x++) {
 			for (var y = 0; y < grid[x].length; y++) {
-				var color = isBorder(x,y,grid) ? 2 : grid[x][y],
+				var color = grid[x][y],
 					idx = (x + y * canvas.width) * 4, // 4 elements per pixel
 					colorParts = BORDER_COLOR;
-				if (color === 0) colorParts = LAND_COLOR;
-				else if (color === 1) colorParts = WATER_COLOR;
+				if (color === 0) colorParts = WATER_COLOR;
+				else if (color === 1) colorParts = LAND_COLOR[0];
+				else if (color === 2) colorParts = LAND_COLOR[1];
 				canvasData.data[idx + 0] = colorParts[0];
 				canvasData.data[idx + 1] = colorParts[1];
 				canvasData.data[idx + 2] = colorParts[2];
@@ -83,7 +73,7 @@ function drawMap() {
 		if (height === undefined) { 
 			genMap(width,width);
 		} else {
-			mapSubGrid(randGrid(8,8),width/16);
+			mapSubGrid(randGrid(4,4,THRESHOLD),width/8);
 		}
 	}
 
@@ -91,18 +81,89 @@ function drawMap() {
 	return false;
 }
 
+/* utility function for iterating over and
+ * updating the value of every square.
+ */
+function mapGrid(grid,func) {
+	newGrid = [];
+	for (var x = 0; x < grid.length; x++) {
+		newGrid[x] = [];
+		for (var y = 0; y < grid[x].length; y++) {
+			newGrid[x][y] = func(grid,x,y);
+		}
+	}
+	return newGrid;
+}
+
+/* generates a random grid of 0s and 1s
+ */
+function randGrid(width,height,threshold) {
+	var arr = [];
+	for (var x = 0; x < width; x++) {
+		arr[x] = [];
+		for (var y = 0; y < height; y++) {
+			arr[x][y] = Math.random() < threshold ? 1 : 0;
+		}
+	}
+	return arr;
+}
+
+/* smooths out the map. replaces all pixels with
+ * their most common neighbor
+ */
+function smooth(grid,n) {
+	function majorityNeighbor(grid,x,y) {
+		var surrounding = neighbors(grid,x,y),
+			val = -3,
+			runLength = -4,
+			mostVal = -5,
+			longestRun = -6;
+		surrounding.sort();
+		for (var n = 0; n < surrounding.length; n++) {
+			if (val == surrounding[n]) {
+				runLength += 1;
+			} else {
+				if (runLength > longestRun) {
+					longestRun = runLength;
+					mostVal = val;
+				}
+				val = surrounding[n];
+				runLength = 1;
+			}
+		}
+		if (runLength > longestRun) {
+			longestRun = runLength;
+			mostVal = val;
+		}
+		return mostVal;
+	}
+	var smoothed = mapGrid(grid,majorityNeighbor);
+	if (n === undefined || n <= 0) return smoothed;
+	else return smooth(smoothed,n-1);
+}
+
+/* draws borders on the map. any land (> 0) square
+ * touching a water square (0) becomes a border.
+ */
+function borderize(grid) {
+	function assignBorder(grid,x,y) { return isBorder(grid,x,y) ? -1 : grid[x][y]; }
+	return mapGrid(grid,assignBorder);
+}
+
+/* cleans up extra borders. makes any border pixels
+ * touching only water or other borders into water.
+ */
+function cleanBorders(grid) {
+	return mapGrid(grid,function(grid,x,y) {
+		return isLonelyBorder(grid,x,y) ? 0 : grid[x][y];
+	});
+}
+
 /* replaces each element in grid with a random
  * one of its neighbors (wrapping on edges)
  */
 function fractalize(grid) {
-	var newGrid = [];
-	for (var x = 0; x < grid.length; x++) {
-		newGrid[x] = [];
-		for (var y = 0; y < grid[x].length; y++) {
-			newGrid[x][y] = randSurrounding(grid,x,y);
-		}
-	}
-	return newGrid;
+	return mapGrid(grid,randSurrounding);
 }
 
 /* returns the neighbors of grid[x][y]
@@ -129,26 +190,35 @@ function randSurrounding(grid,x,y) {
 /* wraps n around the edges if n is 0 or len
  */
 function wrap(n,len) {
-	if (n < 0) {
-		return len-1;
-	} else if (n > len-1) {
-		return 0;
-	} else {
-		return n;
-	}
+	if		(n < 0)		return len-1;
+	else if (n > len-1)	return 0;
+	else				return n;
 }
 
 /* checks if a given 'land' point is bordered by water
  */
-function isBorder(x,y,grid) {
+function isBorder(grid,x,y) {
 	var val = grid[x][y];
-	if (val === 0) {
-		var surrounding = neighbors(grid,x,y),
-			foundDiff = false;
+	if (val > 0) {
+		var surrounding = neighbors(grid,x,y);
 		for (var i = 0; i < surrounding.length; i++) {
-			if (surrounding[i] !== 0) foundDiff = true;
+			if (surrounding[i] === 0) return true;
 		}
-		if (foundDiff) return true;
+	}
+	return false;
+}
+
+/* checks if a given 'border' point is bordered only
+ * by water and other borders
+ */
+function isLonelyBorder(grid,x,y) {
+	var val = grid[x][y];
+	if (val < 0) {
+		var surrounding = neighbors(grid,x,y);
+		for (var i = 0; i < surrounding.length; i++) {
+			if (surrounding[i] > 0) return false;
+		}
+		return true;
 	}
 	return false;
 }
